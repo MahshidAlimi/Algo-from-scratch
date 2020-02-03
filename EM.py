f591e42e3dfdd59daf90343c1a33c2c2fba48615
MahshidAlimi <alimi.mahshid@gmail.com>
2020-02-03 22:35:20 +0000

    Update EM.py

--- EM.py
+++ EM.py
@@ -33,20 +33,21 @@ class EM_Algorithm:
                     feature_counts[index][feature(tple)][class_] += tple_class_dist[class_]
         return class_counts, feature_counts
 
+    # we could add a stopping criteria with a tolerance instead of a predifined number of steps.
     def fit(self, n_steps):
         for step in range(n_steps):
             self.class_counts, self.feature_counts = self.em_step(orig_class_counts=self.class_counts,
                                                                   orig_feature_counts=self.feature_counts)
-
+            
     def log_loss(self, tple):
-        features = self.dataset
+        features = self.data
         result = 0
         count_class = self.class_counts
         count_feature = self.feature_counts
         for count in range(self.num_classes):
             result += prod(count_feature[i][feature(tple)][count]
-                           for (i, feature) in enumerate(feature))/(count_class[count]**len(features)-1)
-        if result > 0 :
-            return -(math.log2(result/len(self.dataset)))
+                           for (i, feature) in enumerate(features))/(count_class[count]**len(features)-1)
+        if result > 0:
+            return -(math.log2(result/len(self.data)))                                   
         else:
             return float("inf")
