0aa89bb5941bb063b3a1b5f9b80f596e05da7577
MahshidAlimi <alimi.mahshid@gmail.com>
2020-02-04 09:02:00 +0000

    Update Expectation Maximization.py

--- Expectation Maximization.py
+++ Expectation Maximization.py
@@ -8,11 +8,26 @@ class EM_Algorithm:
         self.num_classes = num_classes
         self.class_counts = None
         self.feature_counts = None
+        
+    # returns the product of the elements of L
+    @staticmethod
+    def prod(L):
+        res = 1
+        for e in L:
+            res *= e
+        return res
+
+    # generate k random numbers that sum to 1
+    @staticmethod
+    def random_dist(k):
+        res = [random.random() for i in range(k)]
+        s = sum(res)
+        return [v / s for v in res]
 
     # returns a distribution over the classes for the original tuple in the current model
     def prob(self, tple, class_counts, feature_counts):
         features = self.data
-        unnorm = [prod(feature_counts[i][feat(tple)][c]
+        unnorm = [EM_Algorithm.prod(feature_counts[i][feat(tple)][c]
                        for (i, feat) in enumerate(features))/(class_counts[c]**(len(features)-1))
                     for c in range(self.num_classes)]
         return [prob/sum(unnorm) for prob in unnorm]
@@ -26,7 +41,7 @@ class EM_Algorithm:
                 # with no model we will have a random distribution.
                 tple_class_dist = self.prob(tple=tple, class_counts=orig_class_counts, feature_counts=orig_feature_counts)
             else:
-                tple_class_dist = random_dist(self.num_classes)
+                tple_class_dist = EM_Algorithm.random_dist(self.num_classes)
             for class_ in range(self.num_classes):
                 class_counts[class_] += tple_class_dist[class_]
                 for (index, feature) in enumerate(self.data):
